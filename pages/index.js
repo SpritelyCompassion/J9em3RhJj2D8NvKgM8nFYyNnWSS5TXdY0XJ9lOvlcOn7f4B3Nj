d715091e8ef03ba975e408aae4152653ec5f20fc
Aria Nightshade <132257128+SpritelyCompassion@users.noreply.github.com>
2023-05-09 18:41:46 +0800

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,19 @@ const Home = (props) => {
     <>
       <div className="home-container">
         <Head>
-          <title>Muddy Sniveling Mallard</title>
-          <meta property="og:title" content="Muddy Sniveling Mallard" />
+          <title>bTY5AmkdgPpuReFuwBDwGDAOdIUKmsXJCC1RK1KTIXvg1U3k8M</title>
+          <meta
+            name="description"
+            content="XvInzcUTEfPJyqswyRqmgPZILddNebn6upn6bNm2GNAtck90XE0zO5QhkFR1VU5jaFYGDZk1PLRZxtbcnbTCu7askWozMrudj6SWMCeZZc52Ocvood4k7wiqqCdcVTRXLbmn63wZtJvYrqQpyKP0kXeL3XxVtSeh"
+          />
+          <meta
+            property="og:title"
+            content="bTY5AmkdgPpuReFuwBDwGDAOdIUKmsXJCC1RK1KTIXvg1U3k8M"
+          />
+          <meta
+            property="og:description"
+            content="5LD8rvdm9DQqBW87MuNtkjhys7bl9nhklEE1W6CmVEP1dV9qayNwhfZ7RhqvoeMHYeL1xr0cTU8cAc0EZ9Wj927KP0DHogd831rvJSOe7EEak7O6vDCnVqTm3QZitiuaojKYPPUEqk2Q2il7PIkClGCAxjCYa2HGH4FZGmNkGNLUHtgJOznVCxq8VbM2BjeJUel7ItYg"
+          />
         </Head>
         <div className="home-container1">
           <span className="home-text">Living in harmony.</span>
@@ -78,157 +89,163 @@ const Home = (props) => {
       </div>
       <style jsx>
         {`
-          .home-container {
-            width: 100%;
-            display: flex;
-            overflow: auto;
-            min-height: 100vh;
-            align-items: center;
-            flex-direction: column;
-          }
-          .home-container1 {
-            flex: 0 0 auto;
-            width: 100%;
-            height: 100vh;
-            display: flex;
-            position: relative;
-            align-items: center;
-            flex-direction: column;
-            background-size: 61.5vh;
-            justify-content: center;
-            background-image: url('/playground_assets/asnjansw.svg');
-            background-repeat: no-repeat;
-            background-position: center;
-          }
-          .home-text {
-            font-size: 10vh;
-            align-self: center;
-            font-style: normal;
-            font-family: Circular Std Bold;
-            font-weight: 400;
-          }
-          .home-container2 {
-            gap: 12.5vh;
-            flex: 0 0 auto;
-            width: 100%;
-            height: 100vh;
-            display: flex;
-            align-items: center;
-            flex-direction: column;
-            justify-content: center;
-          }
-          .home-text1 {
-            font-size: 4vh;
-            font-style: normal;
-            font-family: Circular Std Book;
-            font-weight: 400;
-          }
-          .home-container3 {
-            gap: 12.5vh;
-            flex: 0 0 auto;
-            width: 100%;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-          }
-          .home-image {
-            width: 10.56375vh;
-            height: 11.12375vh;
-            object-fit: cover;
-          }
-          .home-image1 {
-            width: 12.875vh;
-            height: 12.875vh;
-            object-fit: cover;
-          }
-          .home-image2 {
-            width: 14.36vh;
-            height: 13.71875vh;
-            object-fit: cover;
-          }
-          .home-image3 {
-            width: 9.38vh;
-            height: 8.95875vh;
-            object-fit: cover;
-          }
-          .home-image4 {
-            width: 10.5vh;
-            height: 10.5vh;
-            object-fit: cover;
-          }
-          .home-image5 {
-            width: 18.5vh;
-            height: 12.125vh;
-            object-fit: cover;
-          }
-          .home-container4 {
-            flex: 0 0 auto;
-            width: 100%;
-            height: 100vh;
-            display: flex;
-            position: relative;
-            align-items: center;
-            flex-direction: column;
-            background-size: cover;
-            justify-content: center;
-            background-image: url('/playground_assets/bdinahg.svg');
-            background-repeat: no-repeat;
-            background-position: center;
-          }
-          .home-text2 {
-            z-index: 2;
-            font-size: 4vh;
-            font-style: normal;
-            font-family: Circular Std Book;
-            font-weight: 400;
-          }
-          .home-image6 {
-            top: 0px;
-            left: 0px;
-            right: 0px;
-            width: 52.5vh;
-            bottom: 0px;
-            height: 52.5vh;
-            margin: auto;
-            z-index: 0;
-            position: absolute;
-            object-fit: cover;
-            user-select: none;
-            -ms-user-select: none;
-            -moz-user-select: none;
-            -webkit-user-select: none;
-          }
-          .home-container5 {
-            gap: 2.5vh;
-            flex: 0 0 auto;
-            width: 100%;
-            height: 100vh;
-            display: flex;
-            align-items: center;
-            flex-direction: column;
-            justify-content: center;
-          }
-          .home-link {
-            font-size: 4vh;
-            font-style: normal;
-            font-family: Circular Std Book;
-            font-weight: 400;
-            text-decoration: none;
-          }
-          .home-text3 {
-            font-size: 4vh;
-            font-style: normal;
-            font-family: Circular Std Book;
-            font-weight: 400;
-          }
-          .home-link1 {
-            font-size: 4vh;
-            font-style: normal;
-            font-family: Circular Std Book;
-            font-weight: 400;
-            text-decoration: none;
-          }
-        `}
+.home-container {
+  width: 100%;
+  display: flex;
+  overflow: auto;
+  min-height: 100vh;
+  align-items: center;
+  flex-direction: column;
+}
+.home-container1 {
+  flex: 0 0 auto;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  position: relative;
+  align-items: center;
+  flex-direction: column;
+  background-size: 61.5vh;
+  justify-content: center;
+  background-image: url("/playground_assets/asnjansw.svg");
+  background-repeat: no-repeat;
+  background-position: center;
+}
+.home-text {
+  font-size: 10vh;
+  align-self: center;
+  font-style: normal;
+  font-family: Circular Std Bold;
+  font-weight: 400;
+  font-feature-settings: 'ss01' on, 'salt' on, 'ss02' on, 'ss03' on, 'ss04' on, 'ss05' on, 'ss06' on;
+}
+.home-container2 {
+  gap: 12.5vh;
+  flex: 0 0 auto;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+}
+.home-text1 {
+  font-size: 4vh;
+  font-style: normal;
+  font-family: Circular Std Book;
+  font-weight: 400;
+  font-feature-settings: 'salt' on, 'ss01' on, 'ss02' on, 'ss03' on, 'ss04' on, 'ss05' on;
+}
+.home-container3 {
+  gap: 12.5vh;
+  flex: 0 0 auto;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.home-image {
+  width: 10.56375vh;
+  height: 11.12375vh;
+  object-fit: cover;
+}
+.home-image1 {
+  width: 12.875vh;
+  height: 12.875vh;
+  object-fit: cover;
+}
+.home-image2 {
+  width: 14.36vh;
+  height: 13.71875vh;
+  object-fit: cover;
+}
+.home-image3 {
+  width: 9.38vh;
+  height: 8.95875vh;
+  object-fit: cover;
+}
+.home-image4 {
+  width: 10.5vh;
+  height: 10.5vh;
+  object-fit: cover;
+}
+.home-image5 {
+  width: 18.5vh;
+  height: 12.125vh;
+  object-fit: cover;
+}
+.home-container4 {
+  flex: 0 0 auto;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  position: relative;
+  align-items: center;
+  flex-direction: column;
+  background-size: cover;
+  justify-content: center;
+  background-image: url("/playground_assets/bdinahg.svg");
+  background-repeat: no-repeat;
+  background-position: center;
+}
+.home-text2 {
+  z-index: 2;
+  font-size: 4vh;
+  font-style: normal;
+  font-family: Circular Std Book;
+  font-weight: 400;
+  font-feature-settings: 'salt' on, 'ss01' on, 'ss02' on, 'ss03' on, 'ss04' on, 'ss05' on;
+}
+.home-image6 {
+  top: 0px;
+  left: 0px;
+  right: 0px;
+  width: 52.5vh;
+  bottom: 0px;
+  height: 52.5vh;
+  margin: auto;
+  z-index: 0;
+  position: absolute;
+  object-fit: cover;
+  user-select: none;
+  -ms-user-select: none;
+  -moz-user-select: none;
+  -webkit-user-select: none;
+}
+.home-container5 {
+  gap: 2.5vh;
+  flex: 0 0 auto;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+}
+.home-link {
+  font-size: 4vh;
+  font-style: normal;
+  font-family: Circular Std Book;
+  font-weight: 400;
+  text-decoration: none;
+  font-feature-settings: 'ss01' on, 'salt' on, 'ss02' on, 'ss03' on, 'ss04' on, 'ss05' on;
+}
+.home-text3 {
+  font-size: 4vh;
+  font-style: normal;
+  font-family: Circular Std Book;
+  font-weight: 400;
+  font-feature-settings: ss01' on, 'salt' on, 'ss02' on, 'ss03' on, 'ss04' on, 'ss05' on;
+}
+.home-link1 {
+  font-size: 4vh;
+  font-style: normal;
+  font-family: Circular Std Book;
+  font-weight: 400;
+  text-decoration: none;
+  font-feature-settings: 'salt' on, 'ss01' on, 'ss02' on, 'ss03' on, 'ss04' on, 'ss05' on;
+}
+  `}
       </style>
     </>
   )
